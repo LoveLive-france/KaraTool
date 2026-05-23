20f11a3d8ec715ff8efa0f38335d2c8b977ebae6
Celipot <cyrus.elipot.pro@gmail.com>
2026-05-23 23:08:23 +0200

    fix(): ajouter le json de style dans la compilation de l'appli

--- compile.sh
+++ compile.sh
@@ -3,6 +3,7 @@ NAME=${1:-KaraTool}
 pyinstaller --noconsole --onefile --name "$NAME" \
   --add-data "tools/ffmpeg.exe;." \
   --add-data "src/template.ass;." \
+  --add-data "src/styles_disponibles.json;." \
   --collect-all unidic_lite \
   --collect-all fugashi \
   --collect-all cutlet \
