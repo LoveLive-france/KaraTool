ba7432c26d636f616a8e0af216b4d5618d79a305
Azurys <azurygolo@gmail.com>
2026-05-26 20:40:35 +0200

    Dernière version la plus stable pour la vidéo (oupsie pour les MP3)

--- src/core/player_manager.py
+++ src/core/player_manager.py
@@ -8,26 +8,27 @@ class PlayerManager:
         self.instance = vlc.Instance("--no-video-title-show", "--input-fast-seek")
         self.player = self.instance.media_player_new()
 
-    def load_media(self, image_path, audio_path, subtitle_path=None, duration=None):
+    def load_media(self, image_path, audio_path, subtitle_path=None):
         self.player.stop()
 
+        # 1. On ouvre l'IMAGE comme média principal
+        # Cela force VLC à créer un rendu vidéo
         abs_image_path = str(Path(image_path).absolute())
+        
+        # 2. On prépare les options
         options = []
         
-        # C'EST ICI LA CLÉ :
-        # On dit à VLC de garder l'image affichée pendant la durée du morceau
-        if duration:
-            options.append(f":image-duration={int(duration) + 1}") # +1 pour la sécurité
+        # On attache l'audio en "esclave" (input-slave)
+        if audio_path:
+            audio_uri = Path(audio_path).absolute().as_uri()
+            options.append(f":input-slave={audio_uri}")
         
-        # Attachement audio
-        audio_uri = Path(audio_path).absolute().as_uri()
-        options.append(f":input-slave={audio_uri}")
-        
-        # Sous-titres
+        # On attache les sous-titres
         if subtitle_path and os.path.exists(subtitle_path):
             abs_sub_path = str(Path(subtitle_path).absolute())
             options.append(f":sub-file={abs_sub_path}")
             
+        # 3. Création du média (c'est l'image qui est le média principal)
         media = self.instance.media_new(abs_image_path, *options)
         self.player.set_media(media)
 
