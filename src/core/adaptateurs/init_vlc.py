00c0939b6fb8cf75285280bdd8d639946227eb03
Clément C-G <74550050+clementcga@users.noreply.github.com>
2026-05-30 18:47:25 +0200

    V5.0.2 : Adding new securities (#39)

--- src/core/adaptateurs/init_vlc.py
+++ src/core/adaptateurs/init_vlc.py
@@ -24,13 +24,27 @@ for ruche, sous_cle, acces in chemins_registre:
         cle = winreg.OpenKey(ruche, sous_cle, 0, acces)
         dossier_vlc, _ = winreg.QueryValueEx(cle, "InstallDir")
         winreg.CloseKey(cle)
+
         if os.path.exists(dossier_vlc):
-            os.environ["PYTHON_VLC_MODULE_PATH"] = dossier_vlc
-            vlc_trouve = True
-            break
+            # ON COUPE COURT AUX RECHERCHES DE WINDOWS :
+            # On pointe directement sur le fichier DLL du bon disque (D:, E:, etc.)
+            chemin_direct_dll = os.path.join(dossier_vlc, "libvlc.dll")
+
+            if os.path.exists(chemin_direct_dll):
+                os.environ["PYTHON_VLC_MODULE_PATH"] = dossier_vlc
+                os.environ["PYTHON_VLC_LIB_PATH"] = (
+                    chemin_direct_dll  # <-- Le verrou est ici
+                )
+                vlc_trouve = True
+                break
     except FileNotFoundError:
         continue
 
 if not vlc_trouve:
-    messagebox.showerror("VLC Introuvable", "VLC n'a pas été détecté.")
+    messagebox.showerror(
+        "VLC Introuvable", "Impossible de localiser proprement VLC sur votre système."
+    )
     sys.exit(1)
+
+# Pour satisfaire Ruff (F401) dans main.py
+VLC_READY = True
