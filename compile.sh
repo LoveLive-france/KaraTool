4a51522211fc2b5b768e3fa7165cc2b78a7717ab
Azurys <azurygolo@gmail.com>
2026-07-17 20:57:52 +0200

    Tentative de correction de l'intégration de python dans le build

--- compile.sh
+++ compile.sh
@@ -2,10 +2,16 @@
 NAME=${1:-KaraTool}
 VERSION=${2:-dev}
 echo "__version__ = \"$VERSION\"" > src/version.py
+
+# Trouver le dossier Python
+PYTHON_DIR=$(python -c "import sys; print(sys.base_prefix)")
+
 pyinstaller --noconsole --onefile --name "$NAME" \
   --add-data "tools/ffmpeg.exe;." \
   --add-data "src/template.ass;." \
   --add-data "src/styles_disponibles.json;." \
+  --add-binary "$PYTHON_DIR/python312.dll;." \
+  --add-binary "$PYTHON_DIR/python312d.dll;." \
   --collect-all unidic_lite \
   --collect-all fugashi \
   --collect-all cutlet \
