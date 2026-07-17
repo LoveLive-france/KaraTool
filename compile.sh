73d503b57e4a6e3aead41b931658c2fb65aa6e84
Azurys <azurygolo@gmail.com>
2026-07-17 21:09:38 +0200

    Correction du compile.sh

--- compile.sh
+++ compile.sh
@@ -6,12 +6,19 @@ echo "__version__ = \"$VERSION\"" > src/version.py
 # Trouver le dossier Python
 PYTHON_DIR=$(python -c "import sys; print(sys.base_prefix)")
 
+# Construire les binaires à ajouter
+BINARIES="$PYTHON_DIR/python312.dll;."
+
+# Ajouter la version debug si elle existe
+if [ -f "$PYTHON_DIR/python312d.dll" ]; then
+  BINARIES="$BINARIES --add-binary $PYTHON_DIR/python312d.dll;."
+fi
+
 pyinstaller --noconsole --onefile --name "$NAME" \
   --add-data "tools/ffmpeg.exe;." \
   --add-data "src/template.ass;." \
   --add-data "src/styles_disponibles.json;." \
-  --add-binary "$PYTHON_DIR/python312.dll;." \
-  --add-binary "$PYTHON_DIR/python312d.dll;." \
+  --add-binary $BINARIES \
   --collect-all unidic_lite \
   --collect-all fugashi \
   --collect-all cutlet \
